58996d7ff46f48fb187e0fefe06cfbf37d521567
Ashley <ashleydylanrudd@gmail.com>
2023-02-13 15:44:59 +0000

    v0.2.5

--- examples/testing.cpp
+++ examples/testing.cpp
@@ -2,34 +2,94 @@
 
 #include <iostream>
 
-int main(int argc, char *argv[]) {
 
-	CLIah::Arg test;
-	test.argName = "Verbose";
-	test.primaryMatchStr = "--verbose";
-	test.aliasMatchStr = "-v";
-
-
-	for(int x = 1; x < argc; x++) {
-		std::string inputArg = argv[x];
+/* PLAN
+	send argv and argc to a function, read each arg. if arg is not recognised
+	before one that is, detect and exit.
 	
-		std::cout << inputArg << "        ";
+	if arg is one with substr, just copy next arg and push pointer onwards
+
+
+*/
+
+CLIah::Arg test;
+
+
+void analyse(int argc, char *argv[]) {
+	//Regular argc and argv should have been passed. Adjust to remove argv[0]
+	--argc;
+	++argv;
+
+	//Go through every element in argv
+	for(int argStrIdx = 0; argStrIdx < argc; argStrIdx++) {
+		//Convert curent argv char[] to string
+		std::string inputArg = argv[argStrIdx];
+	
+		std::cout << "input: " << inputArg << std::endl;
 		
-		//TODO Go through all arguments in vector
-		
-		//Create temporary pointer to the current Arg being tested against
+		//Create a temp pointer to the current Arg being tested for
 		CLIah::Arg *tempArg = &test;
 		
-		//TODO if caseSensitive is false, convert both inputArg and tempArg to 
-		//uppercase
+		//Compare the primary and alias arg string to input. Store results
+		int priMatch, aliasMatch;
+		priMatch = tempArg->priMatchStr.compare(inputArg);
+		aliasMatch = tempArg->aliasMatchStr.compare(inputArg);
 		
-		std::cout << "arg:" << tempArg->argName;
-		//Check if argument matches
+		/* NOTE: Leaving primary vs alias detection as possilbe independant 
+		triggers as this may be useful in the future for some niche cases     */
+		if(priMatch == 0 || aliasMatch == 0) {
+			
+			
+			std::cout << "matched reference: " << tempArg->argReference << std::endl;
+			
+			/*** Arg Type detection *******************************************/
+			//Get the matched argument type to do specific execution.
+			CLIah::ArgType tempType = tempArg->type;
+			
+			//If the detected Arg is a Flag Type, just set detected and continue
+			if(tempType == CLIah::ArgType::Flag) {
+				std::cout << "flag set" << std::endl;
+				tempArg->detected = true;
+			}
+			
+			//If the Arg is Subcommand type, get the next inputArg string
+			//set the substring of the main object
+			if(tempType == CLIah::ArgType::Subcommand) {
+				//make sure there *IS* a next argument, if not error.
+				if(argStrIdx + 1 >= argc) {
+					std::cout << "error, no extra command for subcommand" << std::endl;
+					exit(EXIT_FAILURE);
+				}
+				
+				//Incriment argStrIdx by one, to get the next arg string, then
+				//the next loop will skip past it.
+				++argStrIdx;
+				tempArg->substring = argv[argStrIdx];
+				
+				std::cout << "substr: " << tempArg->substring << std::endl;
+				tempArg->detected = true;
+			}
+			
+			//TODO Variable type
+			
+		}
+		
 	
 	}
-	
-	std::cout << std::endl;
 
+}
+
+
+
+int main(int argc, char *argv[]) {
+
+	test.argReference = "Verbose";
+	test.priMatchStr = "--verbose";
+	test.aliasMatchStr = "-v";
+	
+	test.type = CLIah::ArgType::Subcommand;
+
+	analyse(argc, argv);
 
 	return 0;
 }
